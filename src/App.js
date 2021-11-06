4eeb067df65eb476ec868454d240214d2bd8ac78
kahloul <kahloulsami95@gmail.com>
2021-11-06 11:19:42 +0100

    premier hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { moviesData } from "./Composants/MoviesData";
+import AddMovie from "./Composants/AddMovie/AddMovie";
+import MovieList from "./Composants/MoviesList";
+import SearchMovie from "./Composants/SearchMovie/SearchMovie";
 function App() {
+  const [moviesList, setMoviesList] = useState(moviesData);
+  const [nameSearch, setNameSearch] = useState("");
+  const [ratingSearch, setRatingSearch] = useState(null);
+
+  const filterByName = () => {
+    setMoviesList(
+      moviesData.filter((el) =>
+        el.name.toLowerCase().startsWith(nameSearch.toLowerCase().trim())
+      )
+    );
+  };
+
+  const filterByRating = () => {
+    ratingSearch &&
+      setMoviesList(moviesData.filter((el) => el.rating === ratingSearch));
+  };
+
+  useEffect(() => {
+    filterByName();
+  }, [nameSearch]);
+
+  useEffect(() => {
+    filterByRating();
+  }, [ratingSearch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchMovie
+        setNameSearch={setNameSearch}
+        ratingSearch={ratingSearch}
+        setRatingSearch={setRatingSearch}
+      />
+      <MovieList moviesList={moviesList} />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <AddMovie />
+      </div>
     </div>
   );
 }
