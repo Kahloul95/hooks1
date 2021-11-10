d31230f66ba6364a4b6b7324a30c8a83b82a4a9c
kahloul <kahloulsami95@gmail.com>
2021-11-10 10:02:16 +0100

    chekpoint router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { moviesData } from "./Composants/MoviesData";
 import AddMovie from "./Composants/AddMovie/AddMovie";
 import MovieList from "./Composants/MoviesList";
 import SearchMovie from "./Composants/SearchMovie/SearchMovie";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Composants/pages/Home";
+import NotFound from "./Composants/pages/NotFound";
+import MovieDetails from "./Composants/MovieDetails/MovieDetails";
 function App() {
   const [moviesList, setMoviesList] = useState(moviesData);
   const [nameSearch, setNameSearch] = useState("");
@@ -37,18 +41,33 @@ function App() {
 
   return (
     <div className="App">
-      <SearchMovie
-        setNameSearch={setNameSearch}
-        ratingSearch={ratingSearch}
-        setRatingSearch={setRatingSearch}
-      />
-      <MovieList moviesList={moviesList} />
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <AddMovie addNewMovie={addNewMovie} />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies">
+            <SearchMovie
+              setNameSearch={setNameSearch}
+              ratingSearch={ratingSearch}
+              setRatingSearch={setRatingSearch}
+            />
+            <MovieList moviesList={moviesList} />
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <AddMovie addNewMovie={addNewMovie} />
+            </div>
+          </Route>
+          <Route
+            path="/movies/:id"
+            render={(props) => {
+              //console.log(props);
+              return <MovieDetails {...props} movies={moviesList} />;
+            }}
+          />
+          
+          <Route component={NotFound}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
